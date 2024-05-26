564ed9bcca85fef619990eec38aa58140dea52f0
Pulok Ahmed <pulokahmed07@gmail.com>
2024-05-27 00:26:06 +0600

    Added enums,

---
+++ E-CommerceManagementSystem/Data/Enums/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using E_CommerceManageMentSystem.Models;
+
+namespace E_CommerceManageMentSystem.Data.Enums
+{
+    public enum Category
+    {
+        Electronics=1,
+        Apparel,
+        HomeGoods,
+        Toys,
+        Books
+    }
+}
