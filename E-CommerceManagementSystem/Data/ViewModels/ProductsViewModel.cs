2ac70e854027f01d24fc4febebd0aa7eaf07eefd
Pulok Ahmed <pulokahmed07@gmail.com>
2024-05-29 01:50:05 +0600

    Added productviewmodel for product display

---
+++ E-CommerceManagementSystem/Data/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using E_CommerceManageMentSystem.Data.Enums;
+using E_CommerceManageMentSystem.Models;
+using System.Collections.Generic;
+
+namespace E_CommerceManageMentSystem.Data.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
